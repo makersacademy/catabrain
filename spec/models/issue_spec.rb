e13787767b4556085a40ff2cf9089586ef98a993
ddosque <dpbergquist@gmail.com>
2017-03-16 11:21:04 +0000

    fix pending tests

--- spec/models/issue_spec.rb
+++ spec/models/issue_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Issue, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'must have a name' do
+    expect(Issue.new(name: nil)).not_to be_valid
+  end
 end
--- spec/requests/issues_spec.rb
+++ spec/requests/issues_spec.rb
@@ -1,10 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "Issues", type: :request do
+  let(:issue){ Factorygirl.create(:issue) }
   describe "GET /issues" do
     it "works! (now write some real specs)" do
       get issues_path
       expect(response).to have_http_status(200)
     end
+
+
   end
 end
